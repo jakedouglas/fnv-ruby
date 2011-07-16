5f410135b17de2941cd41cb0b93545e22e9a7315
Jake Douglas <jakecdouglas@gmail.com>
2011-07-16 14:58:42 -0700

    multiply mod 2**N instead of masking. way faster

--- lib/fnv.rb
+++ lib/fnv.rb
@@ -3,29 +3,29 @@ class FNV
   INIT64  = 0xcbf29ce484222325
   PRIME32 = 0x01000193
   PRIME64 = 0x100000001b3
-  MASK32  = 0xffffffff
-  MASK64  = 0xffffffffffffffff
+  MOD32   = 2 ** 32
+  MOD64   = 2 ** 64
 
   def fnv1_32(data)
     hash = INIT32
 
     data.bytes.each do |byte|
-      hash = hash * PRIME32
+      hash = (hash * PRIME32) % MOD32
       hash = hash ^ byte
     end
 
-    hash & MASK32
+    hash
   end
 
   def fnv1_64(data)
     hash = INIT64
 
     data.bytes.each do |byte|
-      hash = hash * PRIME64
+      hash = (hash * PRIME64) % MOD64
       hash = hash ^ byte
     end
 
-    hash & MASK64
+    hash
   end
 
   def fnv1a_32(data)
@@ -33,10 +33,10 @@ class FNV
 
     data.bytes.each do |byte|
       hash = hash ^ byte
-      hash = hash * PRIME32
+      hash = (hash * PRIME32) % MOD32
     end
 
-    hash & MASK32
+    hash
   end
 
   def fnv1a_64(data)
@@ -44,9 +44,9 @@ class FNV
 
     data.bytes.each do |byte|
       hash = hash ^ byte
-      hash = hash * PRIME64
+      hash = (hash * PRIME64) % MOD64
     end
 
-    hash & MASK64
+    hash
   end
 end
